e17cbe0aea866f019a49e6ac069080eb8f02b41e
Randheer <randheer094@gmail.com>
2021-01-02 13:49:41 +0800

    Today Leetcode

--- src/test/kotlin/lc_jan/PalindromePermutationTest.kt
+++ src/test/kotlin/lc_jan/PalindromePermutationTest.kt
@@ -1,4 +1,21 @@
 package lc_jan
 
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
 class PalindromePermutationTest {
+    private lateinit var obj: PalindromePermutation
+
+    @Before
+    fun setup() {
+        obj = PalindromePermutation()
+    }
+
+    @Test
+    fun canPermutePalindrome() {
+        Assert.assertFalse(obj.canPermutePalindrome("code"))
+        Assert.assertTrue(obj.canPermutePalindrome("aab"))
+        Assert.assertTrue(obj.canPermutePalindrome("carerac"))
+    }
 }
