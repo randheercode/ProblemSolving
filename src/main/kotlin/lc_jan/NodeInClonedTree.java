3409cb54cbf82a02edd6e296a2f923299e93601d
Randheer <randheer094@gmail.com>
2021-01-03 12:13:34 +0800

    Today Leetcode

---
+++ src/main/kotlin/lc_jan/NodeInClonedTree.java
@@ -0,0 +1,25 @@
+package lc_jan;
+
+// https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/579/week-1-january-1st-january-7th/3590/
+
+
+public class NodeInClonedTree {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        if (cloned == null) return null;
+        if (cloned.val == target.val) return cloned;
+        TreeNode value = getTargetCopy(original, cloned.left, target);
+        if (value != null) return value;
+        value = getTargetCopy(original, cloned.right, target);
+        return value;
+    }
+}
